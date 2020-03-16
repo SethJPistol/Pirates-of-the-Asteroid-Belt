c42d9f42d131b701670c36aef1a97a018df67c56
SethJPistol <sethjpistol@gmail.com>
2020-03-16 21:58:32 +1100

    Screen wrapping foundations set

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -17,9 +17,18 @@ public class Asteroid : MonoBehaviour
 
 	public AsteroidType Type;
 
+	[HideInInspector]
+	public delegate void WrapHandler(Vector3 Position);	//Delegate type to call when wrapping around the screen
+	private WrapHandler Handler = null;					//A handler to hold the function
+
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody>();
+	}
+
+	public void SetWrapHandler(WrapHandler WrapFunction)
+	{
+		Handler = WrapFunction;
 	}
 
 	private void OnCollisionEnter(Collision collision)
@@ -54,6 +63,7 @@ public class Asteroid : MonoBehaviour
 	{
 		Asteroid Asteroid1 = Instantiate(SmallAsteroidPrefab).GetComponent<Asteroid>();
 		Asteroid1.Type = AsteroidType.Small;
+		Asteroid1.SetWrapHandler(Handler);
 		Asteroid1.transform.position = rb.position;
 		Asteroid1.transform.rotation = Quaternion.LookRotation(rb.velocity.normalized);
 		Asteroid1.transform.Rotate(0.0f, 90.0f, 0.0f);
@@ -61,9 +71,16 @@ public class Asteroid : MonoBehaviour
 
 		Asteroid Asteroid2 = Instantiate(SmallAsteroidPrefab).GetComponent<Asteroid>();
 		Asteroid2.Type = AsteroidType.Small;
+		Asteroid2.SetWrapHandler(Handler);
 		Asteroid2.transform.position = transform.position;
 		Asteroid2.transform.rotation = Quaternion.LookRotation(rb.velocity.normalized);
 		Asteroid2.transform.Rotate(0.0f, -90.0f, 0.0f);
 		Asteroid2.rb.velocity = (rb.velocity.magnitude * Asteroid2.transform.forward);
+	}
+
+	private void OnBecameInvisible()
+	{
+		if (Handler != null)
+			Handler(transform.position);
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,21 +8,36 @@ public class GameManager : MonoBehaviour
 
 	public GameObject AsteroidPrefab;
 
-    void Start()
+	private Vector3[] CameraFrustumCorners;
+
+	void Start()
     {
 		Asteroid StartingAsteroid = Instantiate(AsteroidPrefab).GetComponent<Asteroid>();
 		StartingAsteroid.rb.velocity = (new Vector3(0.5f, 0, 0.5f));
-    }
+		StartingAsteroid.SetWrapHandler(Wrap);
+
+		CameraFrustumCorners = new Vector3[4];
+		Camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), 11, Camera.MonoOrStereoscopicEye.Mono, CameraFrustumCorners);
+
+		//Find all asteroids currently in scene and set up their wrap
+	}
 
     // Update is called once per frame
     void Update()
     {
-		Vector3[] FrustumCorners = new Vector3[4];
-		Camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), 11, Camera.MonoOrStereoscopicEye.Mono, FrustumCorners);
+		//DEBUG
 		for (int i = 0; i < 4; i++)
 		{
-			var worldSpaceCorner = Camera.transform.TransformVector(FrustumCorners[i]);
+			var worldSpaceCorner = Camera.transform.TransformVector(CameraFrustumCorners[i]);
 			Debug.DrawRay(Camera.transform.position, worldSpaceCorner, Color.blue);
 		}
+
+
+	}
+
+	public void Wrap(Vector3 Position)
+	{
+		Debug.Log("Wrapped");
+		//Check that the object is still active
 	}
 }
