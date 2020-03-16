bd08f12c8a1b6ae3d40cb0bfa12eeeb1924a4604
SethJPistol <sethjpistol@gmail.com>
2020-03-16 16:46:07 +1100

    Added camera edge detection

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+	public Camera Camera;
+
 	public GameObject AsteroidPrefab;
 
     void Start()
@@ -15,6 +17,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		Vector3[] FrustumCorners = new Vector3[4];
+		Camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), 11, Camera.MonoOrStereoscopicEye.Mono, FrustumCorners);
+		for (int i = 0; i < 4; i++)
+		{
+			var worldSpaceCorner = Camera.transform.TransformVector(FrustumCorners[i]);
+			Debug.DrawRay(Camera.transform.position, worldSpaceCorner, Color.blue);
+		}
+	}
 }
