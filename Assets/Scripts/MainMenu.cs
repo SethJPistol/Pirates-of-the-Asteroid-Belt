dcaf2cb0660b034aaaca3b7264bfe7bee0700251
SethJPistol <sethjpistol@gmail.com>
2020-03-16 11:21:23 +1100

    Added XInput, basic main menu scene loading implemented

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    private XboxController Controller = XboxController.All; //Anybody can use the menu options
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,14 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (XCI.GetButtonDown(XboxButton.A, Controller))
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (XCI.GetButtonDown(XboxButton.B, Controller))
+        {
+            Debug.Log("Quitting!");
+            Application.Quit();
+        }
     }
 }
